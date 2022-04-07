35ea51651d3d947e2c48e84cab91d1c329641872
aterui <hanabi0111@gmail.com>
2022-04-07 18:53:35 -0400

    edit: m_distance

--- R/function_mcsimp.R
+++ R/function_mcsimp.R
@@ -185,6 +185,7 @@ mcsimp <- function(n_species = 5,
 
   if (spatial_env_cor == TRUE) {
 
+    if (is.null(m_distance)) stop("Provide distance matrix to model spatial environmental autocorrelation")
     m_sigma <- var_env * exp(-phi * m_distance)
 
   }else{
