131689c35e53c9991d7ce214c2063e15c30a7591
aterui <hanabi0111@gmail.com>
2022-04-07 08:52:43 -0400

    edit: dispersal, boundary condition

--- R/fun_dispersal.R
+++ R/fun_dispersal.R
@@ -3,6 +3,7 @@
 #' @param x matrix of population sizes
 #' @param v_p_dispersal vector of dispersal probability
 #' @param m_dispersal dispersal matrix
+#' @param boundary_condition Define boundary condition for dispersal. \code{retain} has not loss, \code{loss} induces net loss out of the network.
 #'
 #' @author Akira Terui, \email{hanabi0111@gmail.com}
 #'
@@ -11,7 +12,8 @@
 
 fun_dispersal <- function(x,
                           v_p_dispersal,
-                          m_dispersal) {
+                          m_dispersal,
+                          boundary_condition) {
 
   if (any(diag(m_dispersal) != 0)) stop("error in m_dispersal")
 
@@ -25,17 +27,24 @@ fun_dispersal <- function(x,
   # immigration potential for each patch = m_e_hat x m_dispersal (unit: individuals
   m_i_raw <- m_e_hat %*% m_dispersal
 
-  # v_i_sum: summed across patches
-  v_i_sum <- rowSums(m_i_raw)
+  if (boundary_condition == "retain") {
+    # v_i_sum: summed across patches
+    v_i_sum <- rowSums(m_i_raw)
 
-  # insert 1 if v_i_sum = 0 (to avoid NaN for division)
-  v_i_sum[v_i_sum == 0] <- 1
+    # insert 1 if v_i_sum = 0 (to avoid NaN for division)
+    v_i_sum[v_i_sum == 0] <- 1
 
-  # immigration probability = patch-specific potential / summed potential across patches
-  m_i_prob <- m_i_raw / v_i_sum
+    # immigration probability = patch-specific potential / summed potential across patches
+    m_i_prob <- m_i_raw / v_i_sum
 
-  # expected immigrants: immigration prob. x total emigrants across patches
-  m_i_hat <- m_i_prob * v_e_sum
+    # expected immigrants: immigration prob. x total emigrants across patches
+    m_i_hat <- m_i_prob * v_e_sum
+  }
+
+  if (boundary_condition == "loss") {
+    m_i_hat <- m_i_raw
+  }
+
   m_n_prime <- x + m_i_hat - m_e_hat
 
   return(m_n_prime)
--- R/function_mcsimp.R
+++ R/function_mcsimp.R
@@ -20,6 +20,7 @@
 #' @param xy_coord Each row should correspond to an individual patch, with x and y coordinates (columns). Must be give as a data frame. Defualt \code{NULL}.
 #' @param distance_matrix Distance matrix indicating distance between habitat patches. If provided, the distance matrix will be used to generate dispersal matrix and to calculate distance decay of environmental correlations. Default \code{NULL}.
 #' @param dispersal_matrix Dispersal matrix to be used to simulate dispersal process. Override distance_matrix. Default \code{NULL}.
+#' @param boundary_condition Define boundary condition for dispersal. \code{retain} has not loss, \code{loss} induces net loss out of the network.
 #' @param landscape_size Length of a landscape on a side. Enabled if \code{dispersal_matrix = NULL}.
 #' @param mean_env Temperature at each patch. Length should be one or equal to \code{n_patch}.
 #' @param sd_env Standard deviation of temporal temperature variation at each patch.
@@ -72,6 +73,7 @@ mcsimp <- function(n_species = 5,
                    xy_coord = NULL,
                    distance_matrix = NULL,
                    dispersal_matrix = NULL,
+                   boundary_condition = "retain",
                    landscape_size = 10,
                    mean_env = 0,
                    sd_env = 0,
@@ -269,7 +271,8 @@ mcsimp <- function(n_species = 5,
     ## dispersal, internal function: see "fun_dispersal.R"
     m_n_prime <- fun_dispersal(x = m_n_hat,
                                v_p_dispersal = v_p_dispersal,
-                               m_dispersal = m_dispersal)
+                               m_dispersal = m_dispersal,
+                               boundary_condition = boundary_condition)
 
     ## demographic stochasticity
     m_n <- matrix(rpois(n = n_species * n_patch,
